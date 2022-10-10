f785204430bd2c88d63949347ed18a320ca69927
Brysontech <brightagu2@gmail.com>
2022-10-10 03:12:22 +0100

    update

--- app.js
+++ app.js
@@ -33,12 +33,7 @@ app.use(function (req, res, next) {
   );
   next();
 });
-app.use(
-  cors({
-    origin: "http://localhost:5000",
-    Credential: true,
-  })
-);
+app.use(cors());
 app.use(express.json({ limit: "50mb" }));
 app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
 app.use(cookieParser());
